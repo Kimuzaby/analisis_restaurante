79dc59625f4ef2ceff26f3febc6b7214060bc915
bapatata688 <b0ldm6diego@gmail.com>
2026-04-25 13:57:19 -0600

    estructura de trabajo

--- analisis_restaurante.py
+++ analisis_restaurante.py
@@ -20,6 +20,29 @@ restaurante = encuestado = {
     },
     #
 }
+with open("encuestas_restaurantes.csv", "r") as encuestas:
+    datos_restaurante = csv.DictReader(encuestas)
+    # se llena el diccionario con el contenido de los datos del restaurante
+    # lo valores van convertidos al formato esperado
+    for valor in datos_restaurante:
+        restaurante["id"].append(int(valor["id"]))
+        restaurante["preferencias"]["comida"].append(valor["comida_preferida"])
+        restaurante["preferencias"]["frecuencia"].append(valor["frecuencia_consumo"])
+        restaurante["consumo"]["gasto"].append(float(valor["gasto_promedio"]))
+        restaurante["experiencia"]["producto"].append(
+            int(valor["satisfaccion_producto"])
+        )
+        restaurante["experiencia"]["servicio"].append(
+            int(valor["satisfaccion_servicio"])
+        )
+        restaurante["experiencia"]["tiempo"].append(valor["tiempo_entrega"])
+        restaurante["experiencia"]["precio"].append(valor["precio_percepcion"])
+        restaurante["nps"]["recomendacion"].append(int(valor["recomendaria"]))
+        restaurante["nps"]["volveria"].append(
+            valor["volveria_comprar"].strip().lower() == "true"
+        )
+        restaurante["nps"]["general"].append(int(valor["calificacion_general"]))
+
 """
 estructura esperada:
 encuestado = { 
@@ -44,8 +67,6 @@ encuestado = {
 }
 } 
 """
-with open("encuestas_restaurantes.csv", "r") as encuestas:
-    datos_restaurante = csv.DictReader(encuestas)
 
 
 def reporte_1():
