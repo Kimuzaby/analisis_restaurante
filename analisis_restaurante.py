946dc74e1d854fdced3848fdeb24a6084eabb13c
bapatata688 <b0ldm6diego@gmail.com>
2026-04-24 19:31:10 -0600

    estructura de trabajo

---
+++ analisis_restaurante.py
@@ -0,0 +1,85 @@
+import csv
+
+restaurante = {}
+with open("encuestas_restaurantes.csv", "r") as encuestas:
+    datos_restaurante = csv.DictReader(encuestas)
+
+
+def reporte_1():
+    pass
+
+
+def reporte_2():
+    pass
+
+
+def reporte_3():
+    pass
+
+
+def reporte_4():
+    pass
+
+
+def reporte_5():
+    pass
+
+
+def reporte_6():
+    pass
+
+
+def reporte_7():
+    pass
+
+
+def reporte_8():
+    pass
+
+
+def reporte_9():
+    pass
+
+
+def reporte_10():
+    pass
+
+
+def reporte_11():
+    pass
+
+
+def reporte_12():
+    pass
+
+
+def reporte_13():
+    pass
+
+
+def reporte_14():
+    pass
+
+
+def reporte_15():
+    pass
+
+
+def reporte_16():
+    pass
+
+
+def reporte_17():
+    pass
+
+
+def reporte_18():
+    pass
+
+
+def reporte_19():
+    pass
+
+
+def reporte_20():
+    pass
