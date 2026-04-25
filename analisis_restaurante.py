dcf1edab3adaa9658cd91f0eca8659832738c585
bapatata688 <b0ldm6diego@gmail.com>
2026-04-24 20:49:26 -0600

    estructura de trabajo

--- analisis_restaurante.py
+++ analisis_restaurante.py
@@ -1,6 +1,49 @@
 import csv
 
-restaurante = {}
+restaurante = encuestado = {
+    "id": [],  # id son numeros, deberia ser usando con int()
+    "preferencias": {"comida": [], "frecuencia": []},  # comida deberia ser una
+    # lista de strings de comida
+    # frecuencia deberia ser una lista de srrings de frecuence, ej: semanal, diario, etc
+    "consumo": {"gasto": []},  # gasto deberia ser numeros float
+    "experiencia": {
+        "producto": [],  # producto deberia ser una lista de numeros
+        "servicio": [],  # servicio deberia ser una lista de numeros
+        "tiempo": [],  # tiempo deberia de ser una lista de strings, ej:"Rapido",
+        # "Lento", "medio"
+        "precio": [],  # precio deberia de ser una lista de strings, ej: "medio", "bajo"
+    },
+    "nps": {
+        "recomendacion": [],  # recomendacion sera una lista de numeros
+        "volveria": [],  # volveria deberia ser una lista de booleanos, ej: True, False
+        "general": [],  # general deberia de ser una lista de numeros
+    },
+    #
+}
+"""
+estructura esperada:
+encuestado = { 
+"id": 1, 
+"preferencias": {
+"comida": "Pizza", 
+"frecuencia": "Semanal"
+}, 
+"consumo": {
+"gasto": 50
+},
+"experiencia": { 
+"producto": 8, 
+"servicio": 7, 
+"tiempo": "Rápido", 
+"precio": "Medio" 
+}, 
+"nps": {
+"recomendacion": 9,
+"volveria": True,
+"general": 8 
+}
+} 
+"""
 with open("encuestas_restaurantes.csv", "r") as encuestas:
     datos_restaurante = csv.DictReader(encuestas)
 
